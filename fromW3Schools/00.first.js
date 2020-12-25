fc92a4505af18c7e5d8130753aadb4f6a05f94da
yoonani <yoonani72@gmail.com>
2020-12-25 15:01:14 +0900

    Correction

--- fromW3Schools/00.first.js
+++ fromW3Schools/00.first.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer( function(req, res) {
-  res.Writehead(200, {'Content-Type': 'text.html'});
+  res.writeHead(200, {'Content-Type': 'text.html'});
   res.end('Hello, World!');
 }).listen(8080);
